4962af042669ff1e2d2e8683a0dfee42708c805e
Lawrence Caulker <locaulker@gmail.com>
2025-05-30 13:38:01 -0400

    add checked item functionality

---
+++ src/SingleItem.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+const SingleItem = ({ item, removeItem }) => {
+  const [isChecked, setIsChecked] = useState(item.completed)
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p
+        style={{
+          textTransform: "uppercase",
+          textDecoration: isChecked && "line-through"
+        }}
+      >
+        {item.name}
+      </p>
+      <button type="button" className="btn remove-btn">
+        Delete
+      </button>
+    </div>
+  )
+}
+export default SingleItem
